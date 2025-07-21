af0a38289771c4f4b3bc652cc667ef51e613fb1a
sachin kumar <sachin39430@gmail.com>
2025-07-21 13:13:15 +0530

    optimized

--- src/Auth/appwriteauth.js
+++ src/Auth/appwriteauth.js
@@ -7,7 +7,7 @@ export const client = new Client()
   .setProject(process.env.REACT_APP_APPWRITE_PROJECT_ID);
 
 const account = new Account(client);
-const BASE_URL = `https://${process.env.REACT_APP_VERCEL_URL}`;
+const BASE_URL = `https://${process.env.REACT_APP_VERCEL_PROJECT_PRODUCTION_URL}`;
 console.log("this is base url",BASE_URL);
 
 export const appwriteAuth = {
